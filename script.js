6ce4c3ab8be0f45d58ffdeb26a4bbd4e30d3c634
Matheus <matheuspl92@hotmail.com>
2022-04-29 15:37:43 -0300

    Add commentary about the problem to solve

---
+++ script.js
@@ -0,0 +1,9 @@
+/**Script main objective: Make the user play 5 rounds of "Rock, Paper and Scissors" against the computer.
+ * 
+ * Things that I need to do (ODIN recommendations):
+ * - Write a function named computerPlay() that will randomly return either Rock, Paper or Scissors";
+ * - Write a function that plays a single round of the game and that takes two parameters - playerSelection and computerSelection - and returns a string that declares the winner of the round like so: "You lose! Paper beats Rock!"
+ * OBS: Make sure that the playerSelection parameter is case insensitive! And that the functions returns the result in a function call like console.log(function());
+ * - Write a game() function that calls roundPlay() 5 times and returns the winner of the game at the end!
+ * OBS: Use prompt() to get the user input!;
+ */
