fcaa20c13779287fe5e4c3d2e41ac108d088bd52
Matheus <matheuspl92@hotmail.com>
2022-05-04 21:53:30 -0300

    Removes obsolete code

--- script.js
+++ script.js
@@ -4,17 +4,6 @@ function computerPlay(){
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function userPlay(){
-    let string = prompt("Let's play! Type rock, paper or scissors!").toLowerCase();
-    if(string == "rock"){return 1;}
-    else if (string == "paper"){return 2;}
-    else if (string == "scissors"){return 3;}
-    else {
-        alert("Please type a valid play! Either rock, paper or scissors!");
-        return userPlay();
-    }
-}
-
 function checkVictory(string){
     if (string.slice(0,8) == "You win!") {return true;}
     else if (string.slice(0,8) == "You lose") {return false;}
@@ -39,22 +28,3 @@ function roundPlay(){
     else if(userSelection == 3 && computerSelection == 2){return "You win! Scissors beats Paper!";}
     else if(userSelection == 3 && computerSelection == 3){return "Draw! Both chose Scissors!";}
 }
-
-function game(){
-    let resultGame;
-    let userScore = 0;
-    let computerScore = 0;
-    while(resultGame == undefined){
-        let resultRound = roundPlay();
-        console.log(resultRound);
-        if(checkVictory(resultRound) == undefined){}
-        else if(checkVictory(resultRound) == true){userScore++;}
-        else {computerScore++;}
-
-        resultGame = checkWinner(userScore, computerScore);
-    }
-
-    return resultGame;
-}
-
-console.log(game());
