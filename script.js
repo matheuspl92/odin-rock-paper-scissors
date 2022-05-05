0364fa58139a4d9fe4c35789a3d95c6fe9bcec91
Matheus <matheuspl92@hotmail.com>
2022-05-04 22:26:02 -0300

    Add function to add event listeners to all buttons

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let userScore = 0;
 let computerScore = 0;
 
+addEventListeners();
+
 function computerPlay(){
     let min = Math.ceil(1);
     let max = Math.floor(3);
@@ -30,4 +32,18 @@ function roundPlay(){
     else if(userSelection == 3 && computerSelection == 1){return "You lose! Rock beats Scissors!";}
     else if(userSelection == 3 && computerSelection == 2){return "You win! Scissors beats Paper!";}
     else if(userSelection == 3 && computerSelection == 3){return "Draw! Both chose Scissors!";}
+}
+
+function addEventListeners(){
+    const buttons = Array.from(document.querySelectorAll('button'));
+
+    buttons.forEach(btn => btn.addEventListener('click', event => {
+
+        const btnClass = event.target.className;
+
+        if(btnClass === 'btn-rock'){roundPlay(1);}
+        else if(btnClass === 'btn-paper'){roundPlay(2);}
+        else {roundPlay(3);}
+
+    }))
 }
